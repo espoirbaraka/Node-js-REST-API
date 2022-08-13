6cccf5b5c5525a8f31a358278bb5aef94710cda2
espoirbaraka <esbarakabigeba@gmail.com>
2022-08-13 15:02:46 +0200

    Mysql connection

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -1,2 +1,18 @@
 const mysql = require('mysql');
-const app = mysql();
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "nodejs_db"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
+});
